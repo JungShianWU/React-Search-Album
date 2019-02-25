4e18c3f50585b5162a5df8e3a4a4ca75171d7fad
JungShianWU <jong.shianns@gmail.com>
2019-02-25 18:49:38 +0800

    update
    create react

---
+++ precache-manifest.0a79e30cfd4d15d27ccd1606e94f6171.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5bbe2eba8e942cf503a9",
+    "url": "./static/js/main.5bbe2eba.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "./static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "73f39c6a75594dbb543a8eed1844d417",
+    "url": "./index.html"
+  }
+];
