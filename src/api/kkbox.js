655ff4c2d95132bb0647a1accc67df68da839196
JungShianWU <jong.shianns@gmail.com>
2019-03-12 09:51:01 +0800

    RE-Deploy

--- src/api/kkbox.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-export default axios.create({
-    baseURL: 'https://api.kkbox.com/v1.1',
-    headers: {
-        Authorization: 'Bearer +SQdc4qmlLpgu9uq9V2pEQ=='
-    }
-});
